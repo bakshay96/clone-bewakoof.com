bf3f1c5a4b1d61031a94e5e3c4ceded78c97396a
bakshay96 <akshaymali.edu@gmail.com>
2022-12-16 20:32:27 +0530

    product page

--- bewakoofapp/src/App.js
+++ bewakoofapp/src/App.js
@@ -1,7 +1,19 @@
-function App()
-{
-    <>
-    Thisis react app
-    </>
+import './App.css';
+import Footer from './Components/Footer/Footer';
+import LandingPage from './Components/landingPage';
+import Navbar from './Components/Navbar/Navbar';
+
+
+function App() {
+  return (
+     <div>
+        
+      <Navbar />
+      <LandingPage/>
+      <Footer />
+      
+     
+     </div>
+  );
 }
 export default App;
